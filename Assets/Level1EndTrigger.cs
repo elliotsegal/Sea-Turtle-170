32266533cc6afcd7b3cd03ae5d1bad5d8fe8b7e8
MarkMaTeng <markmateng@gmail.com>
2019-11-01 15:47:15 -0700

    Level Trigger

---
+++ Assets/Level1EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Level1EndTrigger : MonoBehaviour
+{
+    public GameManager gameManager;
+    void OnTriggerEnter()
+    {
+        gameManager.CompleteLevel();
+
+
+        
+    }
+}
